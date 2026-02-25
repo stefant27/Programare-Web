68a4c421eb05dd838253139dcaa56afc83bc1a15
stefant27 <stefan.trimbitas@student.unitbv.ro>
2026-02-25 17:39:11 +0200

    feat: adauga toggle dark/light mode

--- js/contact_form.js
+++ js/contact_form.js
@@ -33,3 +33,17 @@ form.addEventListener('submit', function(event) {
         feedback.style.color = "green";
     }
 });
+
+const themeToggle = document.getElementById('theme-toggle');
+
+themeToggle.addEventListener('click', function(event) {
+    event.preventDefault();
+    
+    document.body.classList.toggle('dark-mode');
+    
+    if (document.body.classList.contains('dark-mode')) {
+        themeToggle.textContent = '☀️ Light Mode';
+    } else {
+        themeToggle.textContent = '🌙 Dark Mode';
+    }
+});
