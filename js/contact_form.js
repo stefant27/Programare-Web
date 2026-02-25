5a6befae42f7043f5cdb45617c6a9108af8d8d19
stefant27 <stefan.trimbitas@student.unitbv.ro>
2026-02-25 16:32:23 +0200

    Lab 1: HTML and CSS review

---
+++ js/contact_form.js
@@ -0,0 +1,13 @@
+
+function submitForm() {
+    
+    const nume = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const mesaj = document.getElementById("message").value;
+
+    console.log("Nume:", nume);
+    console.log("Email:", email);
+    console.log("Mesaj:", mesaj);
+
+    console.warn("Goodbye World!");
+}
