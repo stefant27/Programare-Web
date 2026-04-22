03128e64aa4b3a3c8cd701705efc471740e6c862
stefant27 <stefan.trimbitas@student.unitbv.ro>
2026-04-22 15:10:18 +0300

    feat: Stilizare și link activ

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,11 +1,13 @@
-import { Link } from 'react-router';
+import { NavLink } from 'react-router';
+
 function Navbar() {
  return (
  <nav>
- <Link to="/">Home</Link>
- <Link to="/projects">Proiecte</Link>
- <Link to="/contact">Contact</Link>
+ <NavLink to="/">Home</NavLink>
+ <NavLink to="/projects">Proiecte</NavLink>
+ <NavLink to="/contact">Contact</NavLink>
  </nav>
  );
 }
+
 export default Navbar;
