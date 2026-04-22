3247cf81ddd043ac2cd57eeac6fca6325eaed0c7
stefant27 <stefan.trimbitas@student.unitbv.ro>
2026-04-22 15:04:54 +0300

    feat: meniu navigare cu link + css + modificare app

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 function Home() {
- return (
- <div>
- <h2>Home</h2>
- <p>Bine ai venit pe dashboard-ul meu!</p>
- </div>
- );
+  return (
+    <div className="dashboard-header">
+      <h1>Trimbitas Stefan Ioan</h1>
+      <p>Salut! Sunt un student pasionat de tehnologie web și programare.</p>
+    </div>
+  );
 }
+
 export default Home;
