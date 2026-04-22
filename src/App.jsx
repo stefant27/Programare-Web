6ad991608390f015b6ed3f004324dea56bd9aa63
stefant27 <stefan.trimbitas@student.unitbv.ro>
2026-04-22 14:36:29 +0300

    feat:  Instalare și setup React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,16 @@
-import './App.css';
-import { useState } from 'react';
-import QuickNote from './QuickNote';
-import TodoList from './TodoList';
-import ContactForm from './ContactForm';
-import Clock from './Clock';
-import ProjectList from './ProjectList';
-import UsersList from './UsersList';
-
+import { BrowserRouter, Routes, Route } from 'react-router';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div>
-      <div className="dashboard-header">
-        <h1>Dashboard</h1>
-        <p>Trimbitas Stefan Ioan</p>
-        
-        <h2>Facultatea de inginerie electrica si stiinta calculatoarelor</h2>
-        <p>Student la UNITBV</p>
-        
-        <h3>Despre mine</h3>
-        <p>Salut! Sunt un student pasionat de tehnologie web și programare.</p>
-        <p><strong>Hobby-uri:</strong> tehnologie, muzica, ciclism.</p>
-        
-        <p>Ai apasat de {count} ori</p>
-        <button onClick={() => setCount(count + 1)}>+1</button>
-        <button onClick={() => setCount(count - 1)}>-1</button>
-        <button onClick={() => setCount(0)}>Reset</button>
-      </div>
-
-      <Clock />
-      
-      <ProjectList />
-      
-      <UsersList />
-
-      <QuickNote />
-      <TodoList />
-      <ContactForm />
-    </div>
-  );
+ return (
+ <BrowserRouter>
+ <Routes>
+ <Route path="/" element={<Home />} />
+ <Route path="/projects" element={<Projects />} />
+ <Route path="/contact" element={<Contact />} />
+ </Routes>
+ </BrowserRouter>
+ );
 }
-
 export default App;
