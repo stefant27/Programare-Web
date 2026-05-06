647c746b51b1ff9c05322e90763fa406528d6607
stefant27 <stefan.trimbitas@student.unitbv.ro>
2026-05-06 15:43:00 +0300

    feat: Definire schema și model

---
+++ server/models/Project.js
@@ -0,0 +1,12 @@
+/* eslint-env node */
+const mongoose = require('mongoose');
+
+const projectSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  tech: { type: String, required: true },
+  done: { type: Boolean, default: false },
+});
+
+const Project = mongoose.model('Project', projectSchema);
+
+module.exports = Project;
