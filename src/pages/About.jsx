ee8a07303fa1ee95c196c94202910bcc9d31cc84
stefant27 <stefan.trimbitas@student.unitbv.ro>
2026-05-27 16:40:33 +0300

    feat: nume prin constanta

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,11 @@
 function About() {
+
+const nume = "Stefan Ioan Trimbitas";
+
   return (
     <div className="card">
       <h2>About</h2>
+      <h3>Nume: {nume} </h3>
       <p>Salut! Sunt un student pasionat de tehnologie web și programare.</p>
     </div>
   );
